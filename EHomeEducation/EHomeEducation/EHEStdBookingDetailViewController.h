63c213dd18dc3a21fcd56b0ba36a4474b8d1a822
Qiu Dezheng <846431539@qq.com>
2014-11-25 18:12:34 +0800

    给bookingDetailViewController添加注释

--- EHomeEducation/EHomeEducation/EHEStdBookingDetailViewController.h
+++ EHomeEducation/EHomeEducation/EHEStdBookingDetailViewController.h
@@ -9,26 +9,38 @@
 #import <UIKit/UIKit.h>
 #import "EHETeacher.h"
 @interface EHEStdBookingDetailViewController : UIViewController<UITextViewDelegate>
+//教师名字
 @property(strong,nonatomic)NSString * teacherName;
+//预约时间
 @property(strong,nonatomic)NSString * orderDate;
-@property(strong,nonatomic)EHETeacher * teacher;
-@property(strong,nonatomic)UITextView * descriptonTextField;
-@property(strong,nonatomic)IBOutlet UIScrollView * scrollView;
+@property(strong,nonatomic)EHETeacher * teacher;//一个teacher对象用于传值
+//教师信息label
 @property(strong,nonatomic)UILabel * teacherInfoLabel;
 @property(strong,nonatomic)UILabel * orderLabel;
+//其他信息
 @property(strong,nonatomic)UILabel * otherInfomationLabel;
+//教课起始时间
 @property(strong,nonatomic)UILabel * beginAndEndLabel;
+//授课对象
 @property(strong,nonatomic)UILabel * objectLabel;
+//评价信息
 @property(strong,nonatomic)UILabel * descriptionInfomationLabel;
+//评价等级
 @property(strong,nonatomic)UILabel * rankLabel;
+//家长或者学生进行评价的textView
+@property(strong,nonatomic)UITextView * descriptionTextView;
+//评价的5颗星星，每颗星星都是一个按钮
 @property(strong,nonatomic)UIButton * rankButton1;
 @property(strong,nonatomic)UIButton * rankButton2;
 @property(strong,nonatomic)UIButton * rankButton3;
 @property(strong,nonatomic)UIButton * rankButton4;
 @property(strong,nonatomic)UIButton * rankButton5;
+//用于施加动画效果的星星
 @property(strong,nonatomic)UIButton * rankButton6;
+//确定按钮
 @property(strong,nonatomic)UIButton * buttonSure;
+//继续预约按钮
 @property(strong,nonatomic)UIButton * bookingButton;
-@property(nonatomic)BOOL check;
--(IBAction)touchView:(id)sender;
+@property(nonatomic)BOOL check;//判断现在是不是在进行textView的编辑
+-(IBAction)touchView:(id)sender;//点击背景View的时候所触发的事件
 @end
--- EHomeEducation/EHomeEducation/EHEStdBookingDetailViewController.m
+++ EHomeEducation/EHomeEducation/EHEStdBookingDetailViewController.m
@@ -15,8 +15,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.title=self.teacherName;
     
+    //传之后显示TextView的各种label控件 信息
     _teacherInfoLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 70, 150, 30)];
     [self getLableAttribute:_teacherInfoLabel withText:self.teacherName];
     _teacherInfoLabel.font=[UIFont fontWithName:@"Helvetica-Bold" size:17.0f];
@@ -48,24 +50,30 @@
     _descriptionInfomationLabel.font=[UIFont fontWithName:@"Helvetica-Bold" size:17.0f];
     [self.view addSubview:_descriptionInfomationLabel];
     
-    _descriptonTextField=[[UITextView alloc]initWithFrame:CGRectMake(10, 250, 300, 80)];
-    _descriptonTextField.selectedRange=NSMakeRange(0, 0);
-    _descriptonTextField.delegate=self;
-    _descriptonTextField.text=@"请输入评论信息，不要超过100字......";
-    _descriptonTextField.textColor=[UIColor grayColor];
-    [_descriptonTextField.layer setBorderColor: [[UIColor grayColor] CGColor]];
-    [_descriptonTextField.layer setBorderWidth: 1.0];
-    [_descriptonTextField.layer setCornerRadius:8.0f];
-    [_descriptonTextField.layer setMasksToBounds:YES];
-    [self.view addSubview:_descriptonTextField];
+    //初始化textView并且给TextView指定样式
+    _descriptionTextView=[[UITextView alloc]initWithFrame:CGRectMake(10, 250, 300, 80)];
+    //让textView的光标在起始位置
+    _descriptionTextView.selectedRange=NSMakeRange(0, 0);
+    _descriptionTextView.delegate=self;//绑定textView的代理方法
+    //textView的placeholder即默认值(因为textView没有TextField默认字的的属性)
+    _descriptionTextView.text=@"请输入评论信息，不要超过100字......";
+    _descriptionTextView.textColor=[UIColor grayColor];
+    //给textView设定边框属性信息
+    [_descriptionTextView.layer setBorderColor: [[UIColor grayColor] CGColor]];//边框灰色
+    [_descriptionTextView.layer setBorderWidth: 1.0];//宽度为1
+    [_descriptionTextView.layer setCornerRadius:8.0f];//圆角
+    [_descriptionTextView.layer setMasksToBounds:YES];
+    [self.view addSubview:_descriptionTextView];
     
-    _check=NO;
+    _check=NO;//一开始加载页面的时候用户没有点击编辑textView故而check为NO
+    
     
     _rankLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 330, 150, 30)];
     [self getLableAttribute:_rankLabel withText:@"服务等级"];
     _rankLabel.font=[UIFont fontWithName:@"Helvetica-Bold" size:17.0f];
     [self.view addSubview:_rankLabel];
     
+    //初始化5颗星星
     self.rankButton1=[UIButton buttonWithType:UIButtonTypeSystem];
     [self.rankButton1 setBackgroundImage:[UIImage imageNamed:@"订单-历史评论星星2"] forState:UIControlStateNormal];
     self.rankButton1.frame=CGRectMake(10, 360, 20, 20);
@@ -100,12 +108,14 @@
     self.rankButton5.frame=CGRectMake(290, 360, 20, 20);
     [self.view addSubview:self.rankButton5];
     
+    //初始化完成按钮
     _buttonSure=[UIButton buttonWithType:UIButtonTypeSystem];
     _buttonSure.frame=CGRectMake(20, 390, 100, 37);
     [_buttonSure addTarget: self action:@selector(sureButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
     [_buttonSure setBackgroundImage:[UIImage imageNamed:@"订单-已完成 - 继续预约取消按钮2"] forState:UIControlStateNormal];
     [self.view addSubview:_buttonSure];
     
+    //初始化继续预约按钮
     _bookingButton=[UIButton buttonWithType:UIButtonTypeSystem];
     _bookingButton.frame=CGRectMake(190, 390, 100, 37);
     [_bookingButton addTarget:self action:@selector(bookingButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
@@ -113,16 +123,20 @@
     [self.view addSubview:_bookingButton];
     
 }
+//点击确定按钮所触发的事件
 -(void)sureButtonClicked:(id)sender
 {
     [_buttonSure setBackgroundImage:[UIImage imageNamed:@"订单-已完成 - 继续预约取消按钮"] forState:UIControlStateNormal];
 }
+//点击继续预约按钮所触发的事件
 -(void)bookingButtonClicked:(id)sender
 {
     [_bookingButton setBackgroundImage:[UIImage imageNamed:@"订单-已完成 - 继续预约预约按钮"] forState:UIControlStateNormal];
 }
+//点击第一颗星星的时候触发事件
 -(void)rankButton1Clicked:(id)sender
 {
+    //在外层覆盖一个星星，让本星星和点击的星星边框重合
     self.rankButton6=[UIButton buttonWithType:UIButtonTypeSystem];
     [self.rankButton6 setBackgroundImage:[UIImage imageNamed:@"订单-历史评论星星2"] forState:UIControlStateNormal];
     self.rankButton6.frame=CGRectMake(10, 360, 20, 20);
@@ -131,13 +145,18 @@
     CGRect button6Rect=self.rankButton6.frame;
     button6Rect=CGRectMake(1, 350, 38, 38);
     
+    //得到第一颗星星的frame属性
     CGRect button1Rect=self.rankButton1.frame;
     button1Rect=CGRectMake(9, 358, 24, 24);
     [UIView animateWithDuration:1.0 animations:^{
+        //让所覆盖的星星的宽高变大
+        //让所点击的星星变大，并且切换图片吧
         self.rankButton1.frame=button1Rect;
         self.rankButton6.frame=button6Rect;
+        //渐变消失
         self.rankButton6.alpha=0.0;
         
+        //让其他的星星变小
         self.rankButton2.frame=CGRectMake(80, 360, 20, 20);
         self.rankButton3.frame=CGRectMake(150, 360, 20, 20);
         self.rankButton4.frame=CGRectMake(220, 360, 20, 20);
@@ -149,6 +168,7 @@
     [self turnOffButton:self.rankButton3];
     [self turnOffButton:self.rankButton4];
     [self turnOffButton:self.rankButton5];
+    //一下星星的点击方法类似于第一颗星星，都是动画渐变效果
 }
 -(void)rankButton2Clicked:(id)sender
 {
@@ -300,50 +320,62 @@
 }
 -(void)turnOnButton:(UIButton *)button
 {
+    //点击让所点击的星星变亮
     [button setBackgroundImage:[UIImage imageNamed:@"订单-历史评论星星"] forState:UIControlStateNormal];
 }
 -(void)turnOffButton:(UIButton *)button
 {
+    //点击让其他的星星变暗
     [button setBackgroundImage:[UIImage imageNamed:@"订单-历史评论星星2"] forState:UIControlStateNormal];
 }
+//初始化button的属性
 -(void)initButtonAttribute:(UIButton *)button
 {
     button=[UIButton buttonWithType:UIButtonTypeSystem];
     [button setBackgroundImage:[UIImage imageNamed:@"订单-历史评论星星2"] forState:UIControlStateNormal];
 }
+//点击TextView所触发的事件
 -(IBAction)touchView:(id)sender
 {
     if(_check==YES)
     {
+        //先把所有的label下拉
     [self pushAllLabels];
-    CGRect textFrame=_descriptonTextField.frame;
+        //再上提textView
+    CGRect textFrame=_descriptionTextView.frame;
     textFrame.origin.y+=150;
+        //并且让textView的height变回原始大小
     textFrame.size.height-=20;
-    _descriptonTextField.frame=textFrame;
-    [_descriptonTextField resignFirstResponder];
+    _descriptionTextView.frame=textFrame;
+    [_descriptionTextView resignFirstResponder];//键盘放弃第一响应者身份
         _check=NO;
-        if([_descriptonTextField.text isEqualToString:@""])
+        if([_descriptionTextView.text isEqualToString:@""])//如果用户没有输入任何字符
         {
-            _descriptonTextField.text=@"请输入评论信息，不要超过100字.........";
-            _descriptonTextField.textColor=[UIColor grayColor];
+            _descriptionTextView.text=@"请输入评论信息，不要超过100字.........";//就变回默认值
+            _descriptionTextView.textColor=[UIColor grayColor];
         }
     }
 }
 -(void)textViewDidBeginEditing:(UITextView *)textView
 {
+    //textView刚开始编辑的方法
     textView.text=@"";
+    //如果check为NO
     if(_check==NO)
     {
+        //光标在首
     textView.selectedRange=NSMakeRange(0, 0);
+        //上提所有label控件
     [self pullAllLabels];
     CGRect textFrame=textView.frame;
     textFrame.origin.y-=150;
+        //加大textView的height值
     textFrame.size.height+=20;
     textView.frame=textFrame;
     _check=YES;
     }
 }
-
+//上啦所有label的方法
 -(void)pullAllLabels
 {
     [self pullAllComponents:self.teacherInfoLabel];
@@ -353,6 +385,7 @@
     [self pullAllComponents:self.objectLabel];
     [self pullAllComponents:self.descriptionInfomationLabel];
 }
+//下拉所有label的方法，使所有label的y坐标都还原
 -(void)pushAllLabels
 {
     [self pushAllComponents:self.teacherInfoLabel];
@@ -362,39 +395,44 @@
     [self pushAllComponents:self.objectLabel];
     [self pushAllComponents:self.descriptionInfomationLabel];
 }
+//上拉控件(label)
 -(void)pullAllComponents:(UILabel *)label
 {
     CGRect labelFrame = label.frame;
     labelFrame.origin.y-=150;
     label.frame=labelFrame;
 }
+//下拉控件(label)
 -(void)pushAllComponents:(UILabel *)label
 {
     CGRect labelFrame = label.frame;
     labelFrame.origin.y+=150;
     label.frame=labelFrame;
 }
+
+//添加这个方法，让弹出的键盘点击return的时候可以触发时间
 -(BOOL) textView :(UITextView *) textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *) text {
     
     if ([text isEqualToString:@"\n"]) {
         if(_check==YES)
         {
             [self pushAllLabels];
-            CGRect textFrame=_descriptonTextField.frame;
+            CGRect textFrame=_descriptionTextView.frame;
             textFrame.origin.y+=150;
             textFrame.size.height-=20;
-            _descriptonTextField.frame=textFrame;
-            [_descriptonTextField resignFirstResponder];
+            _descriptionTextView.frame=textFrame;
+            [_descriptionTextView resignFirstResponder];
             _check=NO;
-            if([_descriptonTextField.text isEqualToString:@""])
+            if([_descriptionTextView.text isEqualToString:@""])
             {
-                _descriptonTextField.text=@"请输入评论信息，不要超过100字";
-                _descriptonTextField.textColor=[UIColor grayColor];
+                _descriptionTextView.text=@"请输入评论信息，不要超过100字";
+                _descriptionTextView.textColor=[UIColor grayColor];
             }
         }
     }
     return YES;
 }
+//取得所有label的属性
 -(void)getLableAttribute:(UILabel *)label withText:(NSString *)textName
 {
     label.text=textName;
