d1ce1a14a6094328002a0212559f0c69a53c4269
Qiu Dezheng <846431539@qq.com>
2014-12-01 10:45:25 +0800

    可以在真机测试了

--- EHomeEducation/EHomeEducation/AppDelegate.m
+++ EHomeEducation/EHomeEducation/AppDelegate.m
@@ -315,7 +315,7 @@
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"EHomeEducation" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"EHomeEducation" withExtension:@"mom"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
